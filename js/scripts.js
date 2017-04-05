87b55e3fd0a87b0cebfe033ae12c5b046016ec92
David Shin and Ilene Gorski <david@>
2017-04-05 13:59:09 -0700

    add computer ai functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,106 @@
-var player1Total = 0;
-var player2Total = 0;
-
 function randomize() {
   return Math.ceil(Math.random()*6);
 }
 
+function computerTurn(){
+  computerRoll1=randomize();
+  computerRoll2=randomize();
+  computerRoll3=randomize();
+  if(computerRoll1===1){
+    $("#computer-history").append("<li>"+computerRoll1 + "</li>");
+    computer.turnTotal = 0;
+  }
+  if(computerRoll1 !== 1 && computerRoll2 !==1){
+    $("#computer-history").append("<li>"+computerRoll1 + "</li>");
+    $("#computer-history").append("<li>"+computerRoll2 + "</li>");
+    computer.turnTotal = computerRoll1 + computerRoll2;
+    computer.total += computer.turnTotal;
+    $("#computer-turnTotal").text(computer.turnTotal);
+    if (player1.total - computer.total > 5 && player1.total - computer.total <15) {
+      computerTurn();
+    }
+    $("#computer-total").text(computer.total);
+    console.log("computer",computer);
+  }
+  if (computer.total >= 100) {
+    alert("You lost to a computer!");
+    $("#hit").hide();
+    $("#hold").hide();
+    $("#again").show();
+    player1.total=0;
+    computer.total=0;
+  }
+}
+
+var player1Total = 0;
+var player2Total = 0;
+var computerRoll1 =0;
+var computerRoll2 = 0;
+
+function Player(turnTotal, total) {
+  this.turnTotal = turnTotal;
+  this.total = total;
+}
+var player1 = new Player(0, 0);
+var computer = new Player(0, 0);
+Player.prototype.roll = function(randomNum) {
+  if (randomNum === 1) {
+    this.turnTotal = 0
+    alert("Computer's turn");
+    computerTurn();
+  }
+  else {
+    this.turnTotal += randomNum;
+  }
+}
+
+
+
 $("document").ready(function(){
   $("#solo").click(function(){
     $("#intro").hide();
+    $("#results").show();
     $(".solo").show();
     $("button").show();
-    console.log("hello")
+    $("#again").hide();
   })
   $("#double").click(function(){
     $("#intro").hide();
     $(".double").show();
     $("#double").show();
     $("button").show();
-    console.log("hello")
   })
   $("#hit").click(function(){
-    var randomNum = randomize();
+    randomNum = randomize();
+    $('#player-history').append("<li>" + randomNum + "</li>");
+    player1.roll(randomNum);
+    $("#player-turnTotal").text(player1.turnTotal);
+    console.log(player1,randomNum)
+  });
+  $("#hold").click(function(){
 
+    player1.total+=player1.turnTotal;
+    player1.turnTotal = 0;
+    $("#player-turnTotal").text(player1.turnTotal);
+    $('#player-total').text(player1.total);
 
-    if (randomNum!==1){
-
-      console.log(player1Total,turnTotal);
-      turnTotal+=randomNum
-      player1Total += turnTotal;
-      $("#player-total").text(player1Total);
-      $("#player-history").append("<li>" + randomNum + "</li>");
-      var turnTotal = 0;
+    if(player1.total>=100){
+      alert("You Win!")
+      $("#hit").hide();
+      $("#hold").hide();
+      $("#again").show();
+      player1.total=0;
+      computer.total=0;
+    }else{
+      alert("Computer's turn");
+      computerTurn();
     }
-    else {
-      player1Total-=turnTotal;
-      $("#player-total").text(player1Total);
-      alert("computer's turn");
-      var computerRoll1 = randomize();
-      var computerRoll2 = randomize();
-      if(computerRoll1!==1 && computerRoll2!==1) {
-        player2Total+=(computerRoll1+computerRoll2)
-        $("#computer-total").text(player2Total);
-        $("#computer-history").append("<li>" + computerRoll1 + "</li><li>" + computerRoll2 + "</li>");
-      }else{
-        alert("Your turn!")
-      }
-    }
+  })
+  $("#again").click(function(){
+    $("#results").hide();
+    $("#intro").show();
+    $("#again").hide();
+    $("ul").empty();
+    $("span").empty();
   });
 });
